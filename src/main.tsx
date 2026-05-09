ee5975007e2c90905aaf964b593ec4bc2a475bcd
bor-dot <talatkaya@gmail.com>
2026-05-09 23:20:18 +0300

    Wire Turkish text polishing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './categoryScrollFix';
+import './turkishTextFix';
 import App from './App.tsx';
 import './index.css';
 
